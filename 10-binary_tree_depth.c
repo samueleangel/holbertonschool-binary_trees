38ca45dd06e9b79c1cb94bb72d9218676550fe4f
root <luismichelpcontacto@gmail.com>
2025-04-11 04:17:48 +0000

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,8 +10,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
+
 	if (tree->parent)
 		depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
