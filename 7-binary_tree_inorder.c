40d56aa19c475530443c56e80a5026cd23b5f85a
samueleangel <samueleangel@gmail.com>
2025-04-12 16:18:04 +0100

    Betty Fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
-/** 
+/**
  * binary_tree_inorder - Traverse a binary tree using in-order trasversal
  *
  * @tree: pointer to the root node of the tree to traverse
- * @func: Pointer to the function to call for each node. The value in the 
+ * @func: Pointer to the function to call for each node. The value in the
  * node must be passed as a parameter to this function.
  *
  * If tree or func is NULL, do nothing.
